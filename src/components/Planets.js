27c9146c6e87137560c6fc278b176645e6a57aaa
masonkarsevar <masonkarsevar@gmail.com>
2019-07-17 12:20:47 -0700

    single api calls work just fine. Will need to create a planet array later in the project

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -8,12 +8,18 @@ function Planets() {
         axios 
             .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14')
             .then(response => {
-                console.log(response);
+                console.log(response.data);
             })
             .catch(error => {
                 console.log('error', error);
             })
-    })
+    }, []);
+
+    return (
+        <div className='planets-container'>
+
+        </div>
+    )
 }
 
 export default Planets;
