4347e28a7b847bb3e564661d2b689e9ce078441f
masonkarsevar <masonkarsevar@gmail.com>
2019-07-17 09:00:08 -0700

    api key experiments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from "react";
 import "./App.css";
 
+/*
+// import api key:
+
+Get Your API Key
+Your API key for masonkarsevar@gmail.com is:
+v0uiXBdS2nDZXSwnqMZkNxa1d7014rZx6uL3khd6
+
+// With actual student key:
+https://api.nasa.gov/planetary/apod?api_key=v0uiXBdS2nDZXSwnqMZkNxa1d7014rZx6uL3khd6
+
+// With demo key:
+https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14
+// api example.
+
+
+*/
+
 function App() {
   return (
     <div className="App">
