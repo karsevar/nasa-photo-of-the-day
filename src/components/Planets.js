b1e88b0a0ad9f750a383bde7e6af1f9e20111f4e
masonkarsevar <masonkarsevar@gmail.com>
2019-07-17 15:41:03 -0700

    bugs fixed mvp finished

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -7,34 +7,49 @@ function Planets() {
     const [planet, setPlanet] = useState([]);
     
     useEffect(() => {
-        axios 
-            .get('https://api.nasa.gov/planetary/apod?api_key=v0uiXBdS2nDZXSwnqMZkNxa1d7014rZx6uL3khd6&date=2012-03-14')
-            .then(response => {
 
-                // API check a single object was returned.
-                // console.log(response.data);
+        // Will need to create a for loop to log mulitple planet queries on the dom.
+        const counter = 5;
 
-                // Keeping the useState() object as an array because I'm planning on obtaining 
-                // multiple planet objects in the future.
-                // console.log(response.data);
+        // const dateArray = ['2012-03-14','2012-03-15','2012-03-16','2012-03-17'];
 
-                // Will need to update the planet variable with new response data.
-                // Can't create a planet array within the useEffect() hook.
-                const planetObject = response.data 
-                setPlanet([
-                    planetObject
-                ]);
+        for(let i=0; i < counter; i++) {
 
-                console.log(planetObject);
-                
-            })
-            .catch(error => {
-                console.log('error', error);
-            })
-    }, []);
+            axios 
+                .get(`https://api.nasa.gov/planetary/apod?api_key=v0uiXBdS2nDZXSwnqMZkNxa1d7014rZx6uL3khd6&date=2012-03-${14+i}`)
+                .then(response => {
+
+                    // API check a single object was returned.
+                    // console.log(response.data);
+
+                    // Keeping the useState() object as an array because I'm planning on obtaining 
+                    // multiple planet objects in the future.
+                    // console.log(response.data);
+
+                    // Will need to update the planet variable with new response data.
+                    // Can't create a planet array within the useEffect() hook.
+                    const planetObject = response.data 
+                    setPlanet( previousState => {
+                        return [...previousState, planetObject]
+                    }
+                    );
+
+                    console.log(planetObject);
+                    
+                })
+                .catch(error => {
+                    console.log('error', error);
+                })
+            }
+        
+        }, []); 
+    
+    
+    
 
     return (
         <div className='planets-container'>
+            {console.log('useState', planet)}
             {planet.map(planetType => {
                 return <PlanetCards 
                         date={planetType.date} 
