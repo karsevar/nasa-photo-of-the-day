755339cc3345f239318ceeebd8527a57734dde40
masonkarsevar <masonkarsevar@gmail.com>
2019-07-18 07:55:15 -0700

    styled-coponents installed into modules

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -9,7 +9,7 @@ function Planets() {
     useEffect(() => {
 
         // Will need to create a for loop to log mulitple planet queries on the dom.
-        const counter = 5;
+        const counter = 25;
 
         // const dateArray = ['2012-03-14','2012-03-15','2012-03-16','2012-03-17', '2012-03-18'];
 
@@ -29,7 +29,7 @@ function Planets() {
                     // Will need to update the planet variable with new response data.
                     // Can't create a planet array within the useEffect() hook.
                     const planetObject = response.data 
-                    
+
                     setPlanet( previousState => {
                         return [...previousState, planetObject]
                     }
